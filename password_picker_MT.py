00ac1d29c6ed51e4b1a1995f4af566fd91197d4c
Doctor-Ink <bezusov.88@mail.ru>
2022-11-23 21:24:48 +0300

    FIX: multithreadings

--- password_picker_MT.py
+++ password_picker_MT.py
@@ -18,40 +18,45 @@ PATH = r'C:\Users\Zver\PycharmProjects\RECOVERY_Forgotten_password_EXCEL\book.xl
 
 def combination_generator(password_length, possible_symbols):
     # в этом генероторе проходим все конбинации кроме последней
-    for pass_length in range(password_length[0], password_length[1]):
+    thousand_password = []
+    for pass_length in range(password_length, password_length+1):
             for password in itertools.product(possible_symbols, repeat=pass_length):
                 password = "".join(password)
-                print(password)
-                yield password
-
+                # print(password)
+                thousand_password.append(password)
+                if len(thousand_password) > 99:
+                    print('Возвращаем из генератора')
+                    yield thousand_password
+                    thousand_password = []
+            print('Возвращаем из генератора')
+            yield thousand_password
 
 class Picker(Thread):
-    def __init__(self, PATH, password, *args, **kwargs):
+    def __init__(self, PATH, list_password, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.PATH = PATH
-        self.password = password
+        self.list_password = list_password
 
     def run(self):
-        while True:
-            self.password_entry()
+        self.password_entry()
 
     def password_entry(self):
         # функция запускает клиент и проверяет пароль
 
-        # Сразу перед инициализацией DCOM в run()
-        pythoncom.CoInitializeEx(0)
-        # brute excel doc
-        open_doc = client.Dispatch("Excel.Application")
-        try:
-            open_doc.Workbooks.Open(PATH, False, True, None, self.password)
-            time.sleep(0.1)
-            print(f"[INFO] ---------- Password is: {self.password}")
-            with open('password.txt', mode='w', encoding='utf-8') as file:
-                file.write(self.password)
-            return False
-        except:
-            print(f"Incorrect {self.password}")
-        return True
+        for password in self.list_password:
+            try:
+                # Сразу перед инициализацией DCOM в run()
+                pythoncom.CoInitializeEx(0)
+                # brute excel doc
+                open_doc = client.Dispatch("Excel.Application")
+                open_doc.Workbooks.Open(PATH, False, True, None, password)
+                time.sleep(0.1)
+                print(f"[INFO] ---------- Password is: {password}")
+                with open('password.txt', mode='w', encoding='utf-8') as file:
+                    file.write(password)
+                return False
+            except:
+                print(f"Incorrect {password}")
 
 def input_initial_data():
     # функция запрашивает исходные данные
@@ -86,7 +91,7 @@ def input_initial_data():
 def time_running_script(min_characters, max_characters, possible_symbols):
     total_count = 0
     for step in range(min_characters, max_characters + 1):
-        # print(f'Для пароля из {step} символа(ов) - \n{len(possible_symbols) ** step} комбинаций')
+        print(f'Для пароля из {step} символа(ов) - \n{len(possible_symbols) ** step} комбинаций')
         total_count += len(possible_symbols) ** step
     try:
         time_format = str(datetime.timedelta(seconds= (total_count * 0.18)))
@@ -111,6 +116,14 @@ def get_list_10K():
             my_list_10K.append(line[:-1:])
     return my_list_10K
 
+def get_list_1000():
+    my_list_1000 = []
+    with open('1000_world_password.txt') as file:
+        file.readline()
+        for line in file.readlines():
+            my_list_1000.append(line[:-1:])
+    return my_list_1000
+
 @time_track
 def main():
     # шаг 1 запрос исходных данных
@@ -121,52 +134,31 @@ def main():
         possible_symbols=possible_symbols
     )
 
-    # шаг делим исходный массив данных на 4 потока
-    # исходный массив для первого потока:
-    first = Picker(
-        PATH=PATH,
-        password=combination_generator(password_length=list_length_password[0], possible_symbols=possible_symbols)
-    )
-    second = Picker(
-        PATH=PATH,
-        password=combination_generator(password_length=pass_length, possible_symbols=possible_symbols)
-    )
-    third = Picker(
-        PATH=PATH,
-        password=combination_generator(password_length=pass_length, possible_symbols=possible_symbols)
-    )
-    fourth = Picker(
-        PATH=PATH,
-        password=combination_generator(password_length=pass_length, possible_symbols=possible_symbols)
-    )
+    first = Picker(PATH=PATH, list_password=get_list_150())
+    second = Picker(PATH=PATH, list_password=get_list_10K()[:150])
+    third = Picker(PATH=PATH, list_password=get_list_1000()[:150])
     first.start()
     second.start()
     third.start()
-    fourth.start()
-
     first.join()
     second.join()
     third.join()
-    fourth.join()
 
-
-
+    # шаг 2 делим исходный массив данных на 2 потока
+    # generation = combination_generator(password_length=list_length_password[0], possible_symbols=possible_symbols)
+    #
+    # for thousand_password in generation:
+    #     first = Picker(PATH=PATH, list_password=thousand_password)
+    #     print('First thread is starting')
+    #     first.start()
+    #
+    # for thousand_password in combination_generator(password_length=list_length_password[1], possible_symbols=possible_symbols):
+    #     second = Picker(PATH=PATH, list_password=thousand_password)
+    #     print('Second thread is starting')
+    #     second.start()
 
 
     # шаг 2 - в многопоточном стиле перебираем два списка
-
-    # my_list_10K = get_list_10K()
-    # print(my_list_150)
-    #
-    #
-    # thread_one = Picker(list=my_list_150)
-    # thread_two = Picker(list=my_list_10K)
-    #
-    # thread_one.start()
-    # thread_two.start()
-    #
-    # thread_one.join()
-    # thread_two.join()
 
     # # шаг 3 - перебор списка из 10 000 популярных паролей в мире
     #
