f1d4b33e8f92049f9f64343aeec58f350f893411
Doctor-Ink <bezusov.88@mail.ru>
2022-11-28 23:37:33 +0300

    FIX:multiprocessing

--- password_picker_MP.py
+++ password_picker_MP.py
@@ -1,9 +1,10 @@
 import itertools
+import queue
 import time
 from datetime import datetime
 import win32com.client as client
 from string import digits, punctuation, ascii_letters
-from multiprocessing import Process, Pipe
+from multiprocessing import Process, Event, Pipe, Queue
 import pythoncom
 import datetime
 from password_picker import time_track
@@ -11,33 +12,25 @@ from password_picker import time_track
 
 PATH = r'C:\Users\Zver\PycharmProjects\RECOVERY_Forgotten_password_EXCEL\book.xlsx'
 
-# разделим на 4 потока, пароли будем забирать из генератора
-# В 4-ёxпоточном режиме 100 паролей перебираются   за 18 секунд
-#                       1000 паролей перебираются  за 180 секунд (3 минуты)
-#                       10000 паролей перебираются за 1800 секунд (30 минут)
+# разделим на 3 процесса
+# В 3-ёxппроцессорном режиме
 #                       [INFO] ---------- Password is: 101
-#                               Скрипт отработал -
+#                        Скрипт отработал - 113.81 секунды
+#                        Скрипт отработал - 117.64 секунды
 
 class Picker(Process):
-    def __init__(self, PATH, list_length_password, conn, possible_symbols, *args, **kwargs):
+    def __init__(self, triger, list_length_password, possible_symbols, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.PATH = PATH
+        self.triger = triger
         self.list_length_password = list_length_password
         self.possible_symbols = possible_symbols
-        self.conn = conn
-        self.need_stop = False
-
 
     def run(self):
         for pass_length in range(self.list_length_password[0], self.list_length_password[-1] + 1):
             for password in itertools.product(self.possible_symbols, repeat=pass_length):
                 password = "".join(password)
-                if self.need_stop:
-                    return
                 self.password_entry(password=password)
-
         print('Не удалось найти пароль, возможно вы ввели неверные данные!!!')
-
     def password_entry(self, password):
         # функция запускает клиент и проверяет пароль
         try:
@@ -50,12 +43,40 @@ class Picker(Process):
             print(f"[INFO] ---------- Password is: {password}")
             with open('password.txt', mode='w', encoding='utf-8') as file:
                 file.write(password)
-            self.need_stop = True
-            # будем передавать тригер завершения процесса
-            self.conn.send(self.need_stop)
-            self.conn.close()
+            self.triger.put(True)
         except:
             print(f"Incorrect {password}")
+
+
+class Dispatcher(Process):
+    def __init__(self, possible_symbols, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.all_process = []
+        self.triger = Queue(maxsize=3)
+        self.possible_symbols = possible_symbols
+
+    def add_process(self, list_length_password, ):
+        picker = Picker(triger=self.triger, list_length_password=list_length_password, possible_symbols=self.possible_symbols)
+        self.all_process.append(picker)
+
+    def run(self):
+        print('Родительский класс начал работать')
+        for proc in self.all_process:
+            proc.start()
+        while True:
+            try:
+                # Этот метод у очереди - атомарный и блокирующий,
+                # Поток приостанавливается, пока нет элементов в очереди
+                need_stop = self.triger.get(timeout=1)
+                print('Получили сигнал остановить родительский класс')
+                if need_stop:
+                    for proc in self.all_process:
+                        proc.kill()
+                    break
+            except queue.Empty:
+                print('Ещё не получили пароль')
+
+
 
 def input_initial_data():
     # функция запрашивает исходные данные
@@ -93,7 +114,7 @@ def time_running_script(min_characters, max_characters, possible_symbols):
         print(f'Для пароля из {step} символа(ов) - \n{len(possible_symbols) ** step} комбинаций')
         total_count += len(possible_symbols) ** step
     try:
-        time_format = str(datetime.timedelta(seconds= (total_count * 0.18)))
+        time_format = str(datetime.timedelta(seconds= (total_count * 0.1)))
         print(f"Общее число комбинаций - {total_count}\n "
               f"Расчётное время работы - {time_format} секунд")
     except Exception as exc:
@@ -110,24 +131,14 @@ def main():
         possible_symbols=possible_symbols
     )
 
+    dispatcher = Dispatcher(possible_symbols=possible_symbols)
+    # создаём количество потоков
     list_variant_symbols = [list_length_password[:-2], list_length_password[-2:-1], [list_length_password[-1]]]
-    pickers, pipes = [], []
-    for symbols in list_variant_symbols:
-        parent_conn, child_conn = Pipe()
-        picker = Picker(PATH=PATH, list_length_password=symbols, conn=child_conn, possible_symbols=possible_symbols)
-        pickers.append(picker)
-        pipes.append(parent_conn)
-    for picker in pickers:
-        picker.start()
+    for item in list_variant_symbols:
+        dispatcher.add_process(list_length_password=item)
 
-    for conn in pipes:
-        conn.recv()
-        for picker in pickers:
-            picker.kill()
-        break
-
-    for picker in pickers:
-        picker.join()
+    dispatcher.start()
+    dispatcher.join()
 
 
 if __name__ == '__main__':
